f95b692237534b1ca30e77fa10a8fd45f6878834
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-02-15 14:25:48 +0100

    chore: add build tools for publishing

---
+++ rollup.config.js
@@ -0,0 +1,46 @@
+import typescript from "rollup-plugin-typescript2";
+import babel from "@rollup/plugin-babel";
+import { terser } from "rollup-plugin-terser";
+
+export default [
+  // ES Modules
+  {
+    external: ["query-string"],
+    input: "src/index.ts",
+    output: {
+      file: "dist/index.es.js",
+      format: "es",
+    },
+    plugins: [
+      typescript(),
+      babel({
+        extensions: [".ts"],
+        babelHelpers: "bundled",
+      }),
+    ],
+  },
+
+  // UMD
+  {
+    external: ["query-string"],
+    input: "src/index.ts",
+    output: {
+      file: "dist/index.umd.min.js",
+      format: "umd",
+      name: "urlBuilder",
+      indent: false,
+      globals: {
+        "query-string": "query-string",
+      },
+    },
+    plugins: [
+      typescript(),
+      babel({
+        extensions: [".ts"],
+        exclude: "node_modules/**",
+        babelHelpers: "bundled",
+      }),
+      terser(),
+    ],
+  },
+];
